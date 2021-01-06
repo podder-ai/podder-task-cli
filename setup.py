0be50b7aff2814c527fc6d3eac29ef96be72947a
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2021-01-06 11:22:24 +0900

    Add required module to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="podder-task-cli",
-    install_requires=["click", "poetry", "rich", "toml"],
+    install_requires=["click", "poetry", "rich", "toml", "jinja2"],
     entry_points={
         "console_scripts": [
             "podder=podder_task_cli.cli:main"
