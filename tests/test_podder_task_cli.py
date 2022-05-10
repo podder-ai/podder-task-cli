a6b95ed674b6fd425eef8f4e456e525d57e08453
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2022-05-10 23:17:11 +0700

    Reformat

--- tests/test_podder_task_cli.py
+++ tests/test_podder_task_cli.py
@@ -9,6 +9,7 @@ def test_version():
     file = Path(__file__).parent.parent.joinpath("pyproject.toml")
     data = toml.loads(file.read_text(encoding="utf-8"))
 
-    if "tool" in data and "poetry" in data["tool"] and "version" in data["tool"]["poetry"]:
+    if "tool" in data and "poetry" in data["tool"] and "version" in data[
+            "tool"]["poetry"]:
         project_version = data["tool"]["poetry"]["version"]
         assert __version__ == project_version
